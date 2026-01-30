73d929a81b6e63b4aed505165d0bebf27927c490
Josh Daniel Bañares <joshxfi.dev@gmail.com>
2026-01-31 02:22:29 +0800

    feat: implement translator

--- app/page.tsx
+++ app/page.tsx
@@ -4,57 +4,79 @@ import { useState } from "react";
 import { Streamdown } from "streamdown";
 import { useChat } from "@ai-sdk/react";
 
-import { cn } from "@/lib/utils";
 import { Textarea } from "@/components/ui/textarea";
 import { Field, FieldLabel } from "@/components/ui/field";
 import { Button } from "@/components/ui/button";
 import { Spinner } from "@/components/ui/spinner";
 import { LanguagesIcon } from "lucide-react";
+import { buildPrompt } from "@/lib/utils";
 
 export default function Home() {
   const [input, setInput] = useState("");
-  const { messages, sendMessage, status, stop } = useChat();
+  const { messages, sendMessage, status } = useChat();
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    sendMessage({ text: input });
-    setInput("");
+  const handleSubmit = async () => {
+    sendMessage({
+      text: buildPrompt({
+        text: input,
+        sourceLang: "English",
+        sourceCode: "en",
+        targetLang: "German",
+        targetCode: "de",
+      }),
+    });
   };
 
+  const latestMessage = messages[messages.length - 1];
+
   return (
-    <main className="max-w-2xl flex flex-col min-h-screen mx-auto justify-center items-center">
-      <section className="w-full space-y-2">
-        <Field data-disabled>
-          <FieldLabel htmlFor="textarea-disabled">Message</FieldLabel>
-          <Textarea
-            id="textarea-disabled"
-            placeholder="Type your message here."
-            disabled={status === "streaming"}
-          />
-        </Field>
+    <main className="max-w-4xl flex min-h-screen mx-auto justify-center items-center">
+      <div className="grid grid-cols-2 w-full gap-12">
+        <section className="w-full space-y-2">
+          <Field data-disabled>
+            <FieldLabel htmlFor="textarea-disabled">
+              Translate from English
+            </FieldLabel>
+            <Textarea
+              id="textarea-disabled"
+              onChange={(e) => setInput(e.target.value)}
+              value={input}
+              className="w-full"
+              placeholder="Type your message to translate here."
+              disabled={status === "streaming"}
+            />
+          </Field>
 
-        <Button onClick={handleSubmit} className="cursor-pointer">
-          Translate
-          {status === "submitted" ? <Spinner /> : <LanguagesIcon />}
-        </Button>
-      </section>
+          <Button onClick={handleSubmit} className="cursor-pointer">
+            Translate
+            {status === "submitted" ? <Spinner /> : <LanguagesIcon />}
+          </Button>
+        </section>
 
-      {messages.map((message) => (
-        <div key={message.id}>
-          {message.parts.map((part, i) => {
-            switch (part.type) {
-              case "text":
-                return (
-                  <Streamdown key={`${message.id}-${i}`}>
-                    {part.text}
-                  </Streamdown>
-                );
-              default:
-                return null;
-            }
-          })}
-        </div>
-      ))}
+        <section className="w-full">
+          <Field data-disabled>
+            <FieldLabel>Translate to German</FieldLabel>
+            <div className="border-input bg-input/30 flex field-sizing-content min-h-16 w-full rounded-md border px-3 py-2 text-base shadow-xs md:text-sm">
+              {latestMessage?.role === "assistant" ? (
+                latestMessage?.parts.map((part, i) => {
+                  switch (part.type) {
+                    case "text":
+                      return (
+                        <Streamdown key={`${latestMessage.id}-${i}`}>
+                          {part.text}
+                        </Streamdown>
+                      );
+                    default:
+                      return null;
+                  }
+                })
+              ) : (
+                <p className="text-muted-foreground">Your translated text will appear here.</p>
+              )}
+            </div>
+          </Field>
+        </section>
+      </div>
     </main>
   );
 }
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,24 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+type TranslationConfig = {
+  text: string;
+  sourceLang: string;
+  sourceCode: string;
+  targetLang: string;
+  targetCode: string;
+};
+
+export function buildPrompt(config: TranslationConfig) {
+  const { sourceLang, sourceCode, targetLang, targetCode, text } = config;
+
+  return `You are a professional ${sourceLang} (${sourceCode}) to ${targetLang} (${targetCode}) translator. Your goal is to accurately convey the meaning and nuances of the original ${sourceLang} text while adhering to ${targetLang} grammar, vocabulary, and cultural sensitivities.
+Produce only the ${targetLang} translation, without any additional explanations or commentary. Please translate the following ${sourceLang} text into ${targetLang}:
+
+
+${text}`;
 }
